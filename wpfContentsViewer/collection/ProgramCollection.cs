9d6d0f22ea3d839d1c66c39b5bd1e674469f0726
平尾充一 <hirao@theta-inc.co.jp>
2018-01-14 21:20:01 +0900

    番組の検索のバグ対応&リファクタ

--- wpfContentsViewer/collection/ProgramCollection.cs
+++ wpfContentsViewer/collection/ProgramCollection.cs
@@ -14,7 +14,6 @@ namespace wpfContentsViewer.collection
         public List<Program> listContents;
         public ICollectionView collecion;
 
-        List<int> SearchProgramIds = null;
         List<string> SearchFreeWords = null;
 
         public ProgramCollection(List<Program> myProgramList)
@@ -27,84 +26,41 @@ namespace wpfContentsViewer.collection
 
         public void SetSearchText(string mySearchText)
         {
+            SearchFreeWords = null;
             string[] words = mySearchText.Split(' ');
 
             foreach (string w in words)
             {
-                if (w.IndexOf("P") == 0)
-                {
-                    SearchProgramIds = new List<int>();
-                    string[] ids = w.Substring(1).Split(',');
-
-                    if (ids != null && ids.Length > 0)
-                    {
-                        foreach (string i in ids)
-                        {
-                            try
-                            {
-                                SearchProgramIds.Add(Convert.ToInt32(i));
-                            }
-                            catch (Exception)
-                            {
-
-                            }
-                        }
-                    }
-                }
-                else
-                {
-                    if (SearchFreeWords == null)
-                        SearchFreeWords = new List<string>();
-                    SearchFreeWords.Add(w);
-                }
+                if (SearchFreeWords == null)
+                    SearchFreeWords = new List<string>();
+                SearchFreeWords.Add(w);
             }
         }
 
         public void Execute()
         {
+            collecion.Filter = null;
             bool IsFilterFreeWords = false;
-            bool IsFilterProgramIds = false;
 
             if (SearchFreeWords != null)
                 IsFilterFreeWords = true;
-            if (SearchProgramIds != null)
-                IsFilterProgramIds = true;
 
             collecion.Filter = delegate (object o)
             {
                 Program data = o as Program;
-                if (IsFilterFreeWords || IsFilterProgramIds)
+                if (IsFilterFreeWords)
                 {
-                    if (IsFilterFreeWords)
+                    int m = 0;
+                    foreach (string s in SearchFreeWords)
                     {
-                        bool r = false;
-                        foreach (string s in SearchFreeWords)
-                        {
-                            if (data.Detail.IndexOf(s) >= 0 || data.Name.IndexOf(s) >= 0)
-                                r = true;
-                        }
-                        if (r == false)
-                            return r;
+                        if (data.Name.IndexOf(s) >= 0)
+                            m++;
                     }
-                    if (IsFilterProgramIds)
-                    {
-                        bool r = false;
-                        int pid = 0;
-                        if (data.ChannelId != null && data.ChannelId.Length > 0)
-                        {
-                            pid = Convert.ToInt32(data.ChannelId);
-                        }
-                        foreach (int id in SearchProgramIds)
-                        {
-                            if (pid == id)
-                                r = true;
-                        }
-                        if (r == false)
-                            return r;
-                    }
+                    if (m >= SearchFreeWords.Count)
+                        return true;
                 }
 
-                return true;
+                return false;
             };
         }
     }
